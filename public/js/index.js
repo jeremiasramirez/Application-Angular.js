3c68acdcbb697785a40eb18bac012eed8a003897
Jeremias ramirez <jereramirez499@gmail.com>
2018-11-30 18:40:44 -0400

    Aplicando ng-repeat en etiqueta html

---
+++ public/js/index.js
@@ -0,0 +1,51 @@
+ 
+let modul = function(){
+	let appModule = angular.module('moduleApp',[]);
+
+	appModule.controller('appController',function(){
+
+		 this.pokemons={
+		 	nombre: 'Bulbasaur',
+		 	ides: 1,
+		 	species: 'Seed pokemons',
+		 	tipo: ['Overgrown' , 'Grown'],
+		 	peso: '3.2',
+		 	altura: '6'
+
+		 }
+})
+
+	appModule.controller('appControllerPrice',function(){
+		this.price={
+			priceName: 'Price',
+			priceValue: '12.80'
+		}
+
+	})
+
+
+
+}
+
+modul()
+
+
+
+
+
+
+ 
+ let logoClick = document.getElementById("main__logo__js")
+ 
+ 
+ 	logoClick.addEventListener("click",function(){
+ 	 
+ 	
+ 		if(window.scrollY > 0){
+
+ 				window.scroll(0,0)
+ 			 	
+ 		}
+ 	},false);
+
+ 
